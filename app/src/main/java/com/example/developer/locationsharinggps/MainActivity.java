25e172c606bece16112be08504ec98325eebcf5c
Rene Contreras <ing.renecontreras@cenidet.edu.mx>
2017-07-25 17:07:54 -0500

    Adding Main Activity

--- app/src/main/java/com/example/developer/locationsharinggps/MainActivity.java
+++ app/src/main/java/com/example/developer/locationsharinggps/MainActivity.java
@@ -1,8 +1,17 @@
 package com.example.developer.locationsharinggps;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
 /**
- * Created by developer on 7/25/17.
+ * Created by Rene Contreras on 7/25/17.
  */
 
-public class MainActivity {
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 }
