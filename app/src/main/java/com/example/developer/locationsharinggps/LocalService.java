700a3f9d1166b0e0dc59c952cb83ef6fcdaa38ad
Rene Contreras <ing.renecontreras@cenidet.edu.mx>
2017-07-28 17:21:01 -0500

    Adding service to get location coordinates.

---
+++ app/src/main/java/com/example/developer/locationsharinggps/LocalService.java
@@ -0,0 +1,184 @@
+package com.example.developer.locationsharinggps;
+
+import android.Manifest;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Binder;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.util.Random;
+
+/**
+ * Created by developer on 7/27/17.
+ */
+
+public class LocalService extends Service implements LocationListener {
+    int mStartMode;       // indicates how to behave if the service is killed
+    boolean mAllowRebind = true; // indicates whether onRebind should be used
+    private boolean isServiceRunning;
+    public String lat, lon;
+    private LocationManager locationManager;
+    private String provider;
+
+    private static String TAG = ".//LocalService";
+    private final IBinder mBinder = new LocalBinder();
+    // Random number generator
+    private final Random mGenerator = new Random();
+
+    @Override
+    public void onLocationChanged(Location location) {
+        lat = Double.toString(location.getLatitude());//int lat = (int) (location.getLatitude());
+        lon = Double.toString(location.getLongitude());
+        Log.i(TAG, "LAT: " + lat);
+        Log.i(TAG, "LON: " + lon);
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        Toast.makeText(this, "Enabled new provider " + provider,
+                Toast.LENGTH_SHORT).show();
+
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+
+
+    public class LocalBinder extends Binder {
+        LocalService getService() {
+            // Return this instance of LocalService so clients can call public methods
+            return LocalService.this;
+        }
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        isServiceRunning = true;
+        Log.d(TAG, "onBind");
+
+        return mBinder;
+    }
+
+    /** method for clients */
+    public int getRandomNumber() {
+
+        int random = mGenerator.nextInt(100);
+        Log.d(TAG, "random service: " + random);
+        return random;
+    }
+
+    @Override
+    public void onCreate() {
+        isServiceRunning = true;
+        // The service is being created
+        Log.d(TAG, "onCreate");
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                while (isServiceRunning) {
+                    try {
+                        Thread.sleep(4000);
+                        Log.d(TAG, "hello service: lat: "+lat+"   lon: "+lon);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+
+
+            }
+        }).start();
+
+
+    }
+
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        // The service is starting, due to a call to startService()
+        Log.d(TAG, "onStartCommand");
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        provider = locationManager.getBestProvider(criteria, false);
+
+        Location location = locationManager.getLastKnownLocation(provider);
+        if (location != null) {
+            System.out.println("Provider " + provider + " has been selected.");
+            onLocationChanged(location);
+        } else {
+            Log.i(TAG, "Location not available latitude");
+            Log.i(TAG, "Location not available longitude");
+        }
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+
+        }
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+
+        }
+        locationManager.requestLocationUpdates(provider, 400, 1, this);
+        return mStartMode;
+    }
+
+
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        // All clients have unbound with unbindService()
+        Log.d(TAG, "onUnbind");
+
+        return mAllowRebind;
+    }
+
+
+    @Override
+    public void onRebind(Intent intent) {
+        // A client is binding to the service with bindService(),
+        // after onUnbind() has already been called
+        Log.d(TAG, "onRebind");
+
+    }
+    @Override
+    public void onDestroy() {
+        // The service is no longer used and is being destroyed
+        Log.d(TAG, "onDestroy");
+        isServiceRunning = false;
+
+    }
+}
--- app/src/main/java/com/example/developer/locationsharinggps/MainActivity.java
+++ app/src/main/java/com/example/developer/locationsharinggps/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.developer.locationsharinggps;
 
 import android.Manifest;
+import android.app.ActivityManager;
+import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.content.pm.PackageManager;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -20,47 +25,87 @@ import android.widget.Toast;
  * Created by Rene Contreras on 7/25/17.
  */
 
-public class MainActivity extends AppCompatActivity implements LocationListener {
+public class MainActivity extends AppCompatActivity implements LocationListener, View.OnClickListener {
     public String TAG = "./MainActivity";
+    LocalService mService;
     public String lat, lon;
-    private LocationManager locationManager;
-    private String provider;
+    boolean mBound = false;
+    Button startStopServiceBtn;
+    boolean startStopServiceIsPressed;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button sendLocation = (Button) findViewById(R.id.send_location_button);
-        // Initialize the location fields
-        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        Criteria criteria = new Criteria();
-        provider = locationManager.getBestProvider(criteria, false);
-        Location location = locationManager.getLastKnownLocation(provider);
-        if (location != null) {
-            System.out.println("Provider " + provider + " has been selected.");
-            onLocationChanged(location);
-        } else {
-            Log.i(TAG, "Location not available latitude");
-            Log.i(TAG, "Location not available longitude");
-        }
-
-        sendLocation.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-
-
-
-            }
-        });
+        startStopServiceBtn = (Button) findViewById(R.id.start_tracking_button);
+        startStopServiceBtn.setOnClickListener(this);
     }
 
     @Override
+    protected void onStart() {
+        super.onStart();
+        // Bind to LocalService
+        if (isMyServiceRunning(LocalService.class)){
+            Log.d(TAG, "service is running ");
+            startStopServiceBtn.setText("Stop Tracking");
+            Intent intent = new Intent(this, LocalService.class);
+            bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
+
+        }/*else{
+            Log.d(TAG, "Bind to LocalService and service is being created");
+            Intent intent = new Intent(this, LocalService.class);
+            startService(intent);
+            bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
+
+        }*/
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        // Unbind from the service
+        if (mBound) {
+            unbindService(mConnection);
+            mBound = false;
+            Log.d(TAG, "unbindService ...");
+        }
+    }
+
+    /** Defines callbacks for service binding, passed to bindService() */
+    private ServiceConnection mConnection = new ServiceConnection() {
+
+        @Override
+        public void onServiceConnected(ComponentName className,
+                                       IBinder service) {
+            // We've bound to LocalService, cast the IBinder and get LocalService instance
+            LocalService.LocalBinder binder = (LocalService.LocalBinder) service;
+            mService = binder.getService();
+            mBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName arg0) {
+            mBound = false;
+        }
+    };
+
+    private boolean isMyServiceRunning(Class<?> serviceClass) {
+        ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+            if (serviceClass.getName().equals(service.service.getClassName())) {
+                return true;
+            }
+        }
+        return false;
+    }
+    @Override
     public void onLocationChanged(Location location) {
-        int lat = (int) (location.getLatitude());
-        int lng = (int) (location.getLongitude());
-        Log.i(TAG, "LAT: "+ String.valueOf(lat));
-        Log.i(TAG, "LAT: "+ String.valueOf(lng));
+        lat = Double.toString(location.getLatitude());//int lat = (int) (location.getLatitude());
+        lon = Double.toString(location.getLongitude());
+        Log.i(TAG, "LAT: "+ lat);
+        Log.i(TAG, "LON: "+ lon);
 
     }
 
@@ -84,17 +129,38 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
     @Override
     protected void onResume() {
         super.onResume();
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
-                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.start_tracking_button:
+                {
+                    /*if (isMyServiceRunning(LocalService.class)){
+                        startStopServiceBtn.setText("Stop Tracking");
+                        Log.d(TAG, "service is running ");
+                        Intent intent = new Intent(this, LocalService.class);
+                        bindService(intent, mConnection, Context.BIND_AUTO_CREATE);*/
+                    if (isMyServiceRunning(LocalService.class)){
+                        Log.d(TAG, "service is running ");
+                        stopService(new Intent(getApplicationContext(), LocalService.class));
+                        startStopServiceBtn.setText("Start Tracking");
+                    }
+
+
+                else {
+                        startStopServiceBtn.setText("Stop Tracking");
+                        Log.d(TAG, "Bind to LocalService and service is being created");
+                        Intent intent = new Intent(this, LocalService.class);
+                        startService(intent);
+                        bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
+                    }
+                }
+                break;
+            default:
+                break;
         }
-        locationManager.requestLocationUpdates(provider, 400, 1, this);
+
     }
 }
